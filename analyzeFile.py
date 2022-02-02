cbad1839c641ad6dffd95a8f98e14f558689f754
mateusz <wirus006@gmail.com>
2022-02-02 16:08:34 +0100

    print relations and count of them

--- analyzeFile.py
+++ analyzeFile.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
+import re
+
 with open('network_file_no1.net', 'r') as file:
     data = file.read().replace('\n', ' | ')
 
-print(data[0:1000])
+allRelations = re.findall(r'[0-9]+ \|', data)
+
+relations = set()
+
+for relation in allRelations:
+    relations.add(int(relation[0:-2]))
+
+
+print('\nRelations: ' + str(relations))
+print('\nCount of relations: ' + str(len(relations)) + '\n')
